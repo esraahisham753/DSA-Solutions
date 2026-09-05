6381ec8e5133148f6ba8f67712d154bdcb73ec0e
Esraa Abduallah <esraahisham753@gmail.com>
2026-09-05 19:41:23 +0300

    add docstring for containsDuplicate function

---
+++ 3- containsDuplicate.py
@@ -0,0 +1,14 @@
+"""
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+"""
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+            
+        
+        return len(set(nums)) != len(nums)
+        
