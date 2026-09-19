758cc0aef9d3185655171ceeba2a2ca0cf2a5d03
Esraa Abduallah <esraahisham753@gmail.com>
2026-09-19 15:42:30 +0300

    refactor Fibonacci implementation to use a list for memoization

--- Day 5/1-FibonacciNumber.py
+++ Day 5/1-FibonacciNumber.py
@@ -8,18 +8,29 @@ Given n, calculate F(n).
 
 class Solution:
     def __init__(self):
-        self.res = {
-            0: 0,
-            1: 1
-        }
+        self.res = []
 
-    def fib(self, n: int) -> int:
-        if n in self.res:
+    def fib_rec(self, n):
+        if n == 0:
+            self.res[0] = 0
+            return 0
+        if n == 1:
+            self.res[1] = 1
+            return 1
+        if self.res[n] != 0:
             return self.res[n]
         
-        self.res[n] = self.fib(n-1) + self.fib(n-2)
+        self.res[n] = self.fib_rec(n-1) + self.fib_rec(n-2)
         
         return self.res[n]
+
+    def fib(self, n: int) -> int:
+        self.res = [0] * (n + 1)
+
+        return self.fib_rec(n)
+
+
+        
 
 
         
