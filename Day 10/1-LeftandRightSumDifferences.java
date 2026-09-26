aae0c0aaf612e3e7d14ba8ce8a8cd836ee2f37e5
Esraa Abduallah <esraahisham753@gmail.com>
2026-09-26 12:03:27 +0300

    add implementation for Left and Right Sum Differences problem

---
+++ Day 10/1-LeftandRightSumDifferences.java
@@ -0,0 +1,34 @@
+/* 
+You are given a 0-indexed integer array nums of size n.
+
+Define two arrays leftSum and rightSum where:
+
+leftSum[i] is the sum of elements to the left of the index i in the array nums. If there is no such element, leftSum[i] = 0.
+rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
+Return an integer array answer of size n where answer[i] = |leftSum[i] - rightSum[i]|.
+*/
+
+class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        int n = nums.length;
+        int[] leftSum = new int[n];
+        int[] rightSum = new int[n];
+        int[] answer = new int[n];
+
+        leftSum[0] = 0;
+        for (int i = 1; i < n; i++) {
+            leftSum[i] = leftSum[i - 1] + nums[i - 1]; 
+        }
+
+        rightSum[n - 1] = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            rightSum[i] = rightSum[i + 1] + nums[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = Math.abs(leftSum[i] - rightSum[i]);
+        }
+
+        return answer;
+    }
+}
