6f733922a36c220876e6ada2cde51ea93be47494
Esraa Abduallah <esraahisham753@gmail.com>
2026-09-26 12:11:10 +0300

    add implementation for Pivot Index problem

---
+++ Day 10/2-FindPivotIndex.java
@@ -0,0 +1,35 @@
+/*
+Given an array of integers nums, calculate the pivot index of this array.
+
+The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+
+If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
+
+Return the leftmost pivot index. If no such index exists, return -1.
+*/
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int[] leftSum = new int[n];
+        int[] rightSum = new int[n];
+
+        leftSum[0] = 0;
+        for (int i = 1; i < n; i++) {
+            leftSum[i] = leftSum[i - 1] + nums[i - 1];
+        }
+
+        rightSum[n - 1] = 0;
+        for (int i = n - 2; i >= 0; i--) {
+            rightSum[i] = rightSum[i + 1] + nums[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (leftSum[i] == rightSum[i]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
